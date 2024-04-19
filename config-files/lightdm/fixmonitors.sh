9fc18b8fb41754bdeee03a5c2c1a565dda8e298a
cusniwtt <windywayu2000@gmail.com>
2024-04-20 03:57:33 +0700

    change gdm to lightdm conf

---
+++ config-files/lightdm/fixmonitors.sh
@@ -0,0 +1,2 @@
+xrandr --fb 3000x1920 --output DP-4 --pos 0x0 --rotate left --output DP-0 --mode 1920x1080 --rate 100 --pos 1080x218 --primary
+
---
+++ execute-scripts/fedora-i3wm-init.sh
@@ -0,0 +1,20 @@
+# Update package
+sudo dnf update -y
+# Install dependency for i3
+sudo dnf install @i3-desktop-environment polybar picom viewnior xclip lightdm slick-greeter -y
+
+cp -r ../config-files/i3/ ~/.config/
+cp -r ../config-files/picom/ ~/.config/
+cp -r ../config-files/rofi/ ~/.config/
+cp -r ../config-files/polybar/ ~/.config/
+
+cp -r ../config-files/rofi/fonts/ ~/.fonts/
+fc-cache -f -v
+
+echo "Press mod+shift+r to reload config"
+
+# Change to lightdm
+sudo systemctl enable lightdm.service -f
+sudo cp -r ../config-files/lightdm/ /etc/lightdm/
+echo "Restart to lightdm"
+sudo systemctl restart lightdm
