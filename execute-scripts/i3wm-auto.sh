11286ac6d1b4ad0588729b76070047664b3230ab
cusniwtt <windywayu2000@gmail.com>
2024-04-19 17:13:54 +0700

    auto-conf i3wm

---
+++ execute-scripts/i3wm-auto.sh
@@ -0,0 +1,13 @@
+sudo dnf update -y
+
+sudo dnf install polybar picom -y
+
+cp -r ../config-files/i3/ ~/.config/
+cp -r ../config-files/picom/ ~/.config/
+cp -r ../config-files/rofi/ ~/.config/
+cp -r ../config-files/polybar/ ~/.config/
+
+cp -r ../config-files/rofi/fonts/ ~/.fonts/
+fc-cache -f -v
+
+echo "Press mod+shift+r to reload config"
