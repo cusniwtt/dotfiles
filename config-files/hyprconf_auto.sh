085d7e6dc07f349cafef8846ab534c2a345ecf36
cusniwtt <windywayu2000@gmail.com>
2024-04-19 15:34:10 +0700

    fix rofi conf

--- config-files/hyprconf_auto.sh
+++
@@ -1,4 +0,0 @@
-cp -r ./hypr/ ~/.config/
-cp -r ./wofi/ ~/.config/
-cp -r ./waybar/ ~/.config/ 
-cp -r ./swaylock/ ~/.config/
---
+++ execute-scripts/hyprconf_auto.sh
@@ -0,0 +1,4 @@
+cp -r ../config-files/hypr/ ~/.config/
+cp -r ../config-files/wofi/ ~/.config/
+cp -r ../config-files/waybar/ ~/.config/ 
+cp -r ../config-files/swaylock/ ~/.config/
