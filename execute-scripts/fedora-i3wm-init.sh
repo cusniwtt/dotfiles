b56c2d29f22ffdcb2a3fafda260d76b2e4245cc2
cusniwtt <windywayu2000@gmail.com>
2024-04-28 00:29:27 +0700

    add darkmode gtk-3

--- execute-scripts/fedora-i3wm-init.sh
+++ execute-scripts/fedora-i3wm-init.sh
@@ -1,13 +1,13 @@
 # Update package
 sudo dnf update -y
 # Install dependency for i3
-sudo dnf install @i3-desktop-environment polybar picom viewnior xclip lightdm slick-greeter rofi -y
+sudo dnf install @i3-desktop-environment polybar picom viewnior xclip lightdm slick-greeter rofi python-i3ipc -y
 
 cp -r ../config-files/i3/ ~/.config/
 cp -r ../config-files/picom/ ~/.config/
 cp -r ../config-files/rofi/ ~/.config/
 cp -r ../config-files/polybar/ ~/.config/
-
+cp -r ../config-files/gtk-3.0/ ~/.config/
 cp -r ../config-files/rofi/fonts/ ~/.fonts/
 fc-cache -f -v
 
