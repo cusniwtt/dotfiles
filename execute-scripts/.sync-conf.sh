8d258786a2eafe0236db9355d250141a9c38f6a4
cusniwtt <windywayu2000@gmail.com>
2024-05-11 04:50:12 +0700

    fix hypr

--- execute-scripts/.sync-conf.sh
+++ execute-scripts/.sync-conf.sh
@@ -4,11 +4,11 @@ rsync -avr ~/.config/rofi/ ~/Documents/dot-cheat-sheet/config-files/rofi
 rsync -avr ~/.config/picom/ ~/Documents/dot-cheat-sheet/config-files/picom 
 rsync -avr ~/.config/polybar/ ~/Documents/dot-cheat-sheet/config-files/polybar 
 rsync -avr ~/.config/kitty/ ~/Documents/dot-cheat-sheet/config-files/kitty
-rsync -avr ~/.config/neofetch/ ~/Documents/dot-cheat-sheet/config-files/neofetch
+#rsync -avr ~/.config/neofetch/ ~/Documents/dot-cheat-sheet/config-files/neofetch
 
 # Hyprland
 rsync -avr ~/.config/hypr/ ~/Documents/dot-cheat-sheet/config-files/hypr
-rsync -avr ~/.config/swaylock/ ~/Documents/dot-cheat-sheet/config-files/swaylock
+#rsync -avr ~/.config/swaylock/ ~/Documents/dot-cheat-sheet/config-files/swaylock
 rsync -avr ~/.config/waybar/ ~/Documents/dot-cheat-sheet/config-files/waybar
 rsync -avr ~/.config/wofi/ ~/Documents/dot-cheat-sheet/config-files/wofi
 
