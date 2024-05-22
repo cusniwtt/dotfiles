add0eaefbc358e338830be1ac0bdc0d8ffe9247e
cusniwtt <windywayu2000@gmail.com>
2024-05-23 02:32:13 +0700

    restructure sddm + add splitmon

--- execute-scripts/arch-sddm.sh
+++ execute-scripts/arch-sddm.sh
@@ -1,5 +1,14 @@
-sudo cp ../config-files/sddm/sddm.conf /etc/
-sudo cp ../config-files/sddm/theme.conf /usr/share/sddm/themes/maldives/
+sudo pacman -S qt5-graphicaleffects sddm
+
+# maldives
+sudo cp ../config-files/sddm/maldives/theme.conf /usr/share/sddm/themes/maldives/
 sudo cp ../config-files/wallpapers/arch.jpg /usr/share/sddm/themes/maldives/background.jpg
 
-sudo systemctl restart sddm
+# Slice
+sudo cp -r ../config-files/sddm/sddm-slice/ /usr/share/sddm/themes/
+
+# config-files
+sudo cp ../config-files/sddm/sddm.conf /etc/
+
+sudo systemctl enable sddm.service -f
+sudo systemctl restart sddm.service
