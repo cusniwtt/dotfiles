3336e24b48ba3fda5ba2a4784e042417defa08c3
cusniwtt <windywayu2000@gmail.com>
2024-05-22 19:34:18 +0700

    fix sddm

--- execute-scripts/arch-sddm.sh
+++ execute-scripts/arch-sddm.sh
@@ -1,4 +1,5 @@
-sudo cp ../config-files/sddm.conf /etc/
+sudo cp ../config-files/sddm/sddm.conf /etc/
+sudo cp ../config-files/sddm/theme.conf /usr/share/sddm/themes/maldives/
 sudo cp ../config-files/wallpapers/arch.jpg /usr/share/sddm/themes/maldives/background.jpg
 
 sudo systemctl restart sddm
