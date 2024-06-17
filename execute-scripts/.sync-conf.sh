a7e2687e45259150b0eb0e55bc3e39a88c9747e8
cusniwtt <windywayu2000@gmail.com>
2024-06-17 21:53:50 +0700

    change hyprlock to swaylock + conf fastfetch

--- execute-scripts/.sync-conf.sh
+++ execute-scripts/.sync-conf.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
-# i3wm
-if [ -d ~/.config/i3 ]; then
-    rsync -avr --delete ~/.config/i3/ ~/Documents/dot-cheat-sheet/config-files/i3
-    rsync -avr --delete ~/.config/rofi/ ~/Documents/dot-cheat-sheet/config-files/rofi
-    rsync -avr --delete ~/.config/picom/ ~/Documents/dot-cheat-sheet/config-files/picom
-    rsync -avr --delete ~/.config/polybar/ ~/Documents/dot-cheat-sheet/config-files/polybar
-else
-    echo "i3wm config directory not found. Skipping..."
-fi
-
 # Hyprland
 if [ -d ~/.config/hypr ]; then
     rsync -avr --delete ~/.config/hypr/ ~/Documents/dot-cheat-sheet/config-files/hypr
     rsync -avr --delete ~/.config/waybar/ ~/Documents/dot-cheat-sheet/config-files/waybar
     rsync -avr --delete ~/.config/wofi/ ~/Documents/dot-cheat-sheet/config-files/wofi
     rsync -avr --delete ~/.config/wlogout/ ~/Documents/dot-cheat-sheet/config-files/wlogout
+    rsync -avr --delete ~/.config/swaylock/ ~/Documents/dot-cheat-sheet/config-files/swaylock
 else
     echo "Hyprland config directory not found. Skipping..."
 fi
