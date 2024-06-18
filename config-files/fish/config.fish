b7a971673a9f460d6fdb0f792f7b63c915724b58
cusniwtt <windywayu2000@gmail.com>
2024-06-18 21:44:52 +0700

    add code to wayland env

--- config-files/fish/config.fish
+++ config-files/fish/config.fish
@@ -17,3 +17,17 @@ alias pubip='curl ifconfig.co/'
 eval "$(starship init fish)"
 
 alias sync-conf=~/.sync-conf.sh
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/cusniwtt/miniconda3/bin/conda
+    eval /home/cusniwtt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/home/cusniwtt/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/home/cusniwtt/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/cusniwtt/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
--- dot-files/.profile
+++ dot-files/.profile
@@ -1,3 +1,4 @@
 export BROWSER=google-chrome-stable
 export TERM=xterm-kitty
 export QT_QPA_PLATFORMTHEME="qt5ct"
+export ELECTRON_OZONE_PLATFORM_HINT=auto code
