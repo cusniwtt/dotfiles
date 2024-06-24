b65637a1ed1654fb7ace389ee007704d6c63b327
Wayu Ragwongsiri <windywayu2000@gmail.com>
2024-06-25 01:07:40 +0700

    Update arch-hyprland.sh

--- execute-scripts/arch-hyprland.sh
+++ execute-scripts/arch-hyprland.sh
@@ -2,11 +2,11 @@
 # You need to install Hyprland From https://github.com/JaKooLit/Arch-Hyprland
 # This script is only dotfiles and configs config-files
 
-sudo pacman -S waybar hyprpaper cliphist swaylock wofi yazi ffmpegthumbnailer unarchiver jq poppler fd ripgrep fzf zoxide grim slurp swappy
+sudo pacman -S waybar hyprpaper cliphist wofi yazi ffmpegthumbnailer unarchiver jq poppler fd ripgrep fzf zoxide grim slurp swappy
 paru -S pyprland
 
 # Copy Config files
 cp -r ../config-files/hypr/ ~/.config/
 cp -r ../config-files/wofi/ ~/.config/
 cp -r ../config-files/waybar/ ~/.config/ 
-cp -r ../config-files/swaylock/ ~/.config/
+#cp -r ../config-files/swaylock/ ~/.config/
