3f8965d122df6dab4bf8c06aac65ca24ecce08e6
cusniwtt <windywayu2000@gmail.com>
2024-06-25 03:12:16 +0700

    fix waybar + autostart

--- config-files/waybar/launch.sh
+++ config-files/waybar/launch.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
-#
-# Quit waybar
-killall waybar
+#!/usr/bin/env sh
 
+# Terminate already running bar instances
+killall -q waybar
+
+# Wait until the processes have been shut down
+while pgrep -x waybar >/dev/null; do sleep 1; done
+
+# Launch main
 waybar
