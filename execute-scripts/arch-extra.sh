9491e12d578f088c78c86dfa5e25444545d4eb79
cusniwtt <windywayu2000@gmail.com>
2024-06-27 02:36:14 +0700

    back to chrome

--- execute-scripts/arch-extra.sh
+++ execute-scripts/arch-extra.sh
@@ -1,3 +1,3 @@
 #paru -S thorium-browser vesktop spotify-launcher
 sudo pacman -S discord firefox spotify-launcher vlc gimp obs-studio virtualbox
-paru arch-gaming-meta visual-studio-code-bin
+paru -S google-chrome cachyos-gaming-meta visual-studio-code-cli-bin
