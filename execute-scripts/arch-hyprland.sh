316b1634534888b32085acb266544366cca4e1a1
cusniwtt <windywayu2000@gmail.com>
2024-07-01 02:13:06 +0700

    add waypaper GUI

--- execute-scripts/arch-hyprland.sh
+++ execute-scripts/arch-hyprland.sh
@@ -3,7 +3,7 @@
 # This script is only dotfiles and configs config-files
 
 sudo pacman -S waybar hyprpaper cliphist wofi yazi ffmpegthumbnailer unarchiver jq poppler fd ripgrep fzf zoxide grim slurp swappy
-paru -S pyprland
+paru -S pyprland waypaper
 
 # Copy Config files
 cp -r ../config-files/hypr/ ~/.config/
