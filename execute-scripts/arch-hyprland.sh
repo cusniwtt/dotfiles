72d3ff0240286bb5b08856d4c6a97aa18d40f8c6
cusniwtt <windywayu2000@gmail.com>
2024-07-01 21:51:38 +0700

    add ly by default

--- execute-scripts/arch-hyprland.sh
+++ execute-scripts/arch-hyprland.sh
@@ -2,7 +2,7 @@
 # You need to install Hyprland From https://github.com/JaKooLit/Arch-Hyprland
 # This script is only dotfiles and configs config-files
 
-sudo pacman -S waybar hyprpaper cliphist wofi yazi ffmpegthumbnailer unarchiver jq poppler fd ripgrep fzf zoxide grim slurp swappy
+sudo pacman -S waybar hyprpaper cliphist wofi yazi ffmpegthumbnailer unarchiver jq poppler fd ripgrep fzf zoxide grim slurp swappy qt5-wayland qt6-wayland
 paru -S pyprland waypaper
 
 # Copy Config files
@@ -11,3 +11,9 @@ cp -r ../config-files/wofi/ ~/.config/
 cp -r ../config-files/waybar/ ~/.config/ 
 cp -r ../config-files/swaylock/ ~/.config/
 cp -r ../config-files/wlogout/ ~/.config/
+
+# Enable ly as SDDM
+sudo pacman -S ly
+sudo cp ../config-files/ly/config.ini /etc/ly/
+sudo systemctl enable ly.service -f
+sudo systemctl restart ly.service
