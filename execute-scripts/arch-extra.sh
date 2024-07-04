a636ce6fa5799f52675fe0bdcc6c510cbf936734
cusniwtt <windywayu2000@gmail.com>
2024-07-04 23:16:38 +0700

    remove discord and use floorp + pwa instead

--- execute-scripts/arch-extra.sh
+++ execute-scripts/arch-extra.sh
@@ -1,10 +1,14 @@
 echo "Install basic utility software"
-sudo pacman -S firefox spotify-launcher vlc gimp obs-studio
+sudo pacman -S spotify-launcher vlc gimp obs-studio
+# Use Floorp instead firefox for pwa apps.
+paru -S floorp
 
 # If this meta package is bloat. Install what u want. (Steam, protonupqt etc)
 echo "Install gaming-meta"
+echo "In Arch will error (meta from cachyos). So, install each app instead."
+echo "or install with aur arch-gaming-meta"
 sudo pacman -S cachyos-gaming-meta
-sudo pacman -S discord
+#sudo pacman -S discord   # Use floorp in pwa mode because electron suck!!!
 
 # This need some step to complete. Watch instruction reference
 # reference -> https://computingforgeeks.com/install-kvm-qemu-virt-manager-arch-manjar/
