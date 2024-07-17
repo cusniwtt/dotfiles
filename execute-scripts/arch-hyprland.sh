26ad8f4e2c1f6a3e2736b66d46fc95165d71a20a
cusniwtt <windywayu2000@gmail.com>
2024-07-17 22:46:41 +0700

    change wofi style

--- execute-scripts/arch-hyprland.sh
+++ execute-scripts/arch-hyprland.sh
@@ -3,7 +3,7 @@
 # This scrit is not installation script, Before run this script
 # You need to install Hyprland From https://github.com/JaKooLit/Arch-Hyprland
 # This script is only dotfiles and configs config-files
-sudo pacman -S waybar hyprpaper cliphist wofi nemo grim slurp swappy nwg-look feh
+sudo pacman -S waybar hyprpaper hyprpicker cliphist wofi nemo grim slurp swappy nwg-look feh
 sudo pacman -S qt5-wayland qt6-wayland mako network-manager-applet polkit-kde-agent
 paru -S pyprland waypaper xwaylandvideobridge hyprshade
 
