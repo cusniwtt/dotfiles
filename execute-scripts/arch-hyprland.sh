472f225e5c41d50097eaf5ae26067c0222bf6416
Wayu Ragwongsiri <windywayu2000@gmail.com>
2024-07-19 17:02:25 +0700

    Update arch-hyprland.sh

--- execute-scripts/arch-hyprland.sh
+++ execute-scripts/arch-hyprland.sh
@@ -3,8 +3,8 @@
 # This scrit is not installation script, Before run this script
 # You need to install Hyprland From https://github.com/JaKooLit/Arch-Hyprland
 # This script is only dotfiles and configs config-files
-sudo pacman -S waybar hyprpaper cliphist wofi nemo grim slurp swappy nwg-look feh
-sudo pacman -S qt5-wayland qt6-wayland mako network-manager-applet polkit-kde-agent
+sudo pacman -S waybar hyprpaper cliphist wofi nemo grim slurp swappy nwg-look feh wireplumber \
+    qt5-wayland qt6-wayland mako network-manager-applet polkit-kde-agent
 paru -S pyprland waypaper hyprshade hyprpicker
 
 # Copy Config files
