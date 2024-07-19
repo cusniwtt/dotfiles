3f89f5c057ad9b3148e5357fe37a58e1e8741645
cusniwtt <windywayu2000@gmail.com>
2024-07-19 19:59:07 +0700

    add gamemode

--- execute-scripts/arch-extra.sh
+++ execute-scripts/arch-extra.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 
 echo "Install basic utility software"
-sudo pacman -S spotify-launcher vlc gimp obs-studio
+sudo pacman -S spotify-launcher vlc obs-studio
 paru -S thorium-browser-bin
 
 # If this meta package is bloat. Install what u want. (Steam, protonupqt etc)
 echo "Install gaming-meta"
 echo "In Arch will error (meta from cachyos). So, install each app instead."
 echo "or install with aur arch-gaming-meta"
-sudo pacman -S discord steam gamemode
-
-# This need some step to complete. Watch instruction reference
-# reference -> https://computingforgeeks.com/install-kvm-qemu-virt-manager-arch-manjar/
-echo "Install Virtual Machine software"
-sudo pacman -Syyu
-sudo pacman -S archlinux-keyring
-sudo pacman -S qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat dmidecode ebtables iptables libguestfs
-sudo systemctl enable libvirtd.service
-sudo systemctl start libvirtd.service
-
-# for VPN and remote desktop
-echo "Install VPN and RDP"
-sudo pacman -S openfortivpn remmina freerdp
+sudo pacman -S discord steam gamemode lib32-gamemode
+sudo usermod -aG gamemode $(whoami)
+gamemoded -t
