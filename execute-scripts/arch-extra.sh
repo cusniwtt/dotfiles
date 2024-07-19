733636bdc44cffb188807d17e6d43d341faf8d53
Wayu Ragwongsiri <windywayu2000@gmail.com>
2024-07-19 17:01:40 +0700

    Update arch-extra.sh

--- execute-scripts/arch-extra.sh
+++ execute-scripts/arch-extra.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "Install basic utility software"
-sudo pacman -S spotify-launcher vlc gimp obs-studio firefox
+sudo pacman -S spotify-launcher vlc gimp obs-studio
+paru -S thorium-browser-bin
 
 # If this meta package is bloat. Install what u want. (Steam, protonupqt etc)
 echo "Install gaming-meta"
 echo "In Arch will error (meta from cachyos). So, install each app instead."
 echo "or install with aur arch-gaming-meta"
-#sudo pacman -S cachyos-gaming-meta     # For CachyOS
-sudo pacman -S discord steam lutris
+sudo pacman -S discord steam gamemode
 
 # This need some step to complete. Watch instruction reference
 # reference -> https://computingforgeeks.com/install-kvm-qemu-virt-manager-arch-manjar/
