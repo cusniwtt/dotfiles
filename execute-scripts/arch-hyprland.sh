819fd028df73b63330631ac550bd7f32fa51de99
cusniwtt <windywayu2000@gmail.com>
2024-08-14 22:00:40 +0700

    add wlogout hyprlock

--- execute-scripts/arch-hyprland.sh
+++ execute-scripts/arch-hyprland.sh
@@ -5,7 +5,7 @@
 # This script is only dotfiles and configs config-files
 yay -S waybar hyprpaper cliphist wofi nemo grim slurp swappy nwg-look eog wireplumber \
   qt5-wayland qt6-wayland mako network-manager-applet polkit-kde-agent pyprland waypaper \
-  hyprshade hyprpicker nemo-fileroller nemo-preview
+  hyprshade hyprpicker nemo-fileroller nemo-preview wlogout hyprlock
 
 # Copy Config files
 cp -r ../config-files/hypr/ ~/.config/
