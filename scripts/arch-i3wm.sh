256db92d869820ec303a8927565c0af659adff92
cusniwtt <windywayu2000@gmail.com>
2024-08-27 01:47:16 +0700

    add dunst to i3

--- scripts/arch-i3wm.sh
+++ scripts/arch-i3wm.sh
@@ -1,2 +1,2 @@
-sudo yay -S i3-wm i3status-rust mako picom udisks nemo nemo-fileroller nemo-preview \
+sudo yay -S i3-wm i3status-rust dunst picom udisks2 nemo nemo-fileroller nemo-preview \
   network-manager-applet eog wireplumber polkit-gnome autotiling feh xorg-xinput
