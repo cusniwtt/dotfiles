5438fd42488ee72b537858bbd4528fcdf11613d5
cusniwtt <windywayu2000@gmail.com>
2025-02-07 03:54:29 +0700

    add i3wm config

--- scripts/arch-hyprland.sh
+++ scripts/arch-hyprland.sh
@@ -2,6 +2,7 @@
 
 yay -S hyprland hyprpaper hyprpicker hyprlock xdg-desktop-portal-hyprland hyprsysteminfo hyprpolkitagent \
   kitty waybar cliphist wofi nemo grim slurp swappy nwg-look eog wireplumber qt5-wayland qt6-wayland \
-  mako pyprland waypaper hyprshade nemo-fileroller nemo-preview wlogout network-manager-applet
+  mako pyprland waypaper hyprshade nemo-fileroller nemo-preview wlogout network-manager-applet \
+  capitaine-cursors numix-circle-icon-theme-git materia-gtk-theme arc-gtk-theme
 
 #sudo systemctl enable ly.service
--- scripts/arch-i3wm.sh
+++ scripts/arch-i3wm.sh
@@ -1,2 +1,2 @@
-sudo yay -S i3-wm i3status-rust dunst picom udisks2 nemo nemo-fileroller nemo-preview \
-  network-manager-applet eog wireplumber polkit-gnome autotiling feh xorg-xinput
+sudo yay -S i3 i3status-rust dunst picom udisks2 nemo nemo-fileroller nemo-preview \
+  network-manager-applet eog wireplumber polkit-gnome autotiling feh xorg-xinput flameshot
