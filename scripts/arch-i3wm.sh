7e094059ffac2f8353811b68d215080b7306d8a3
cusniwtt <windywayu2000@gmail.com>
2025-02-07 03:57:11 +0700

    add more config

--- scripts/arch-i3wm.sh
+++ scripts/arch-i3wm.sh
@@ -1,2 +1,4 @@
 sudo yay -S i3 i3status-rust dunst picom udisks2 nemo nemo-fileroller nemo-preview \
-  network-manager-applet eog wireplumber polkit-gnome autotiling feh xorg-xinput flameshot
+  network-manager-applet eog wireplumber polkit-gnome autotiling feh xorg-xinput \
+  flameshot nwg-look capitaine-cursors numix-circle-icon-theme-git materia-gtk-theme \
+  arc-gtk-theme
