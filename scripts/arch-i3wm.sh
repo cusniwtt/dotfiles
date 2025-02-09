af8557bb1923bfefa080a5b3dc7f0138e4e695db
cusniwtt <windywayu2000@gmail.com>
2025-02-10 02:33:09 +0700

    add clipmenu + udiskit

--- scripts/arch-i3wm.sh
+++ scripts/arch-i3wm.sh
@@ -1,4 +1,3 @@
-sudo yay -S i3 i3status-rust dunst picom udisks2 nemo nemo-fileroller nemo-preview \
-  network-manager-applet eog wireplumber polkit-gnome autotiling feh xorg-xinput \
-  flameshot nwg-look capitaine-cursors numix-circle-icon-theme-git materia-gtk-theme \
-  arc-gtk-theme
+yay -S i3 i3status-rust dunst picom udisks2 nemo nemo-fileroller nemo-preview \
+  eog wireplumber polkit-gnome autotiling feh xorg-xinput flameshot nwg-look \
+  numix-circle-icon-theme-git materia-gtk-theme arc-gtk-theme clipmenu
