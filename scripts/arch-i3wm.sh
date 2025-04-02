b368a551de39b7a7c614c246bb6e007a1b82beba
cusniwtt <windywayu2000@gmail.com>
2025-04-02 15:59:40 +0700

    remove unuse theme

--- scripts/arch-i3wm.sh
+++ scripts/arch-i3wm.sh
@@ -1,3 +1,3 @@
 yay -S i3 i3status-rust dunst picom udisks2 nemo nemo-fileroller nemo-preview \
   eog wireplumber polkit-gnome autotiling feh xorg-xinput flameshot nwg-look \
-  numix-circle-icon-theme-git materia-gtk-theme arc-gtk-theme clipmenu
+  clipmenu
