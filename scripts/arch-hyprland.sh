9b039bd97d8ada430a81b90ff0a40a8f603480e2
cusniwtt <windywayu2000@gmail.com>
2025-04-02 15:59:04 +0700

    remove unused package

--- scripts/arch-hyprland.sh
+++ scripts/arch-hyprland.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-yay -S hyprland hyprpaper hyprpicker hyprlock xdg-desktop-portal-hyprland hyprsysteminfo hyprpolkitagent \
-  kitty waybar cliphist wofi nemo grim slurp swappy nwg-look eog wireplumber qt5-wayland qt6-wayland \
-  mako waypaper hyprshade nemo-fileroller nemo-preview wlogout iwgtk numix-circle-icon-theme-git \
-  materia-gtk-theme arc-gtk-theme
+yay -S hyprland hyprpaper hyprpicker hyprlock xdg-desktop-portal-hyprland hyprsysteminfo \
+  hyprpolkitagent kitty waybar cliphist wofi nemo grim slurp swappy nwg-look eog wireplumber \
+  qt5-wayland qt6-wayland mako waypaper hyprshade nemo-fileroller nemo-preview wlogout iwgtk
 
 #sudo systemctl enable ly.service
