7482c3d9448767d3ec04ed209c23a49ff3bc10e4
cusniwtt <windywayu2000@gmail.com>
2025-05-04 15:26:32 +0700

    add hyprlock blur

--- scripts/fedora-hyprland.sh
+++ scripts/fedora-hyprland.sh
@@ -1,4 +1,4 @@
 sudo dnf copr enable solopasha/hyprland -y
 sudo dnf install hyprland hyprpicker hyprlock xdg-desktop-portal-hyprland hyprsysteminfo swww \
   hyprpolkitagent kitty waybar cliphist wofi thunar grim slurp swappy wireplumber pavucontrol \
-  qt5ct qt6ct mako waypaper wlogout file-roller eog nwg-panel -y
+  qt5ct qt6ct mako waypaper wlogout file-roller eog nwg-panel ImageMagick -y
