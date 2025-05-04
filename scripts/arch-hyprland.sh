718b570fceb15428911ebb765bcb48e0bbeb6e5d
Wayu Ragwongsiri <windywayu2000@gmail.com>
2025-05-04 20:06:53 +0700

    Update arch-hyprland.sh

--- scripts/arch-hyprland.sh
+++ scripts/arch-hyprland.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 yay -S hyprland hyprpicker hyprlock xdg-desktop-portal-hyprland hyprsysteminfo hyprpolkitagent \
-  swww kitty waybar cliphist wofi nemo grim slurp swappy nwg-look eog wireplumber imagemagick \
-  qt5-wayland qt6-wayland mako waypaper hyprshade nemo-fileroller nemo-preview wlogout iwgtk
+  swww kitty waybar cliphist wofi thunar grim slurp swappy nwg-look eog wireplumber imagemagick \
+  qt5-wayland qt6-wayland mako waypaper hyprshade file-roller wlogout iwgtk
 
 #sudo systemctl enable ly.service
