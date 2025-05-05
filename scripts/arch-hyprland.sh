bd733473320db9261e6a1c7f4bef9a39f652d73f
cusniwtt <windywayu2000@gmail.com>
2025-05-05 18:10:50 +0700

    remove iwgtk and use nmtui instead

--- scripts/arch-hyprland.sh
+++ scripts/arch-hyprland.sh
@@ -2,6 +2,6 @@
 
 yay -S hyprland hyprpicker hyprlock xdg-desktop-portal-hyprland hyprsysteminfo hyprpolkitagent \
   swww kitty waybar cliphist wofi thunar grim slurp swappy nwg-look eog wireplumber imagemagick \
-  qt5-wayland qt6-wayland mako waypaper hyprshade file-roller wlogout iwgtk
+  qt5-wayland qt6-wayland mako waypaper hyprshade file-roller wlogout thunar-archive-plugin
 
 #sudo systemctl enable ly.service
