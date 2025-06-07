1cda01c26c9f02f4ca02af35d91631eb54f5cf54
cusniwtt <windywayu2000@gmail.com>
2025-06-07 13:06:30 +0700

    add usage application

--- scripts/arch-i3wm.sh
+++ scripts/arch-i3wm.sh
@@ -1,3 +1,3 @@
 yay -S i3 i3status-rust dunst picom udisks2 nemo nemo-fileroller nemo-preview \
   eog polkit-gnome autotiling feh xorg-xinput flameshot nwg-look pavucontrol \
-  clipmenu gnome-terminal
+  clipmenu gnome-terminal gnome-keyring
