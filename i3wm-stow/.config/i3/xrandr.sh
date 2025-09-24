03aa00c8809457084d1fbbdc28d19c1bd4f53a96
cusniwtt <windywayu2000@gmail.com>
2025-09-25 00:08:32 +0700

    add xrandr + zshrc

--- i3wm-stow/.config/i3/xrandr.sh
+++ i3wm-stow/.config/i3/xrandr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
-xrandr --fb 3000x1920 \
-  --output DP-0 --mode 1920x1080 --pos 1080x200 --primary --rate 100 \
-  --output DP-4 --pos 0x0 --rotation left --mode 1920x1080
+xrandr --output DP-2 --mode 1920x1080 --rate 100
+#xrandr --fb 3000x1920 \
+#  --output DP-0 --mode 1920x1080 --pos 1080x200 --primary --rate 100 \
+#  --output DP-4 --pos 0x0 --rotation left --mode 1920x1080
