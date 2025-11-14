9861efca884074be6f0625aea2678f33ec92372a
cusniwtt <windywayu2000@gmail.com>
2025-11-14 23:05:26 +0700

    using mate-polkit

--- scripts/arch-i3wm.sh
+++ scripts/arch-i3wm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-yay -S i3 i3status-rust dunst picom udisks2 thunar gvfs thunar-archive-plugin xarchiver \
-  ffmpegthumbnailer tumbler libgsf eog lxsession feh xorg-xinput flameshot nwg-look pavucontrol \
-  clipmenu xfce4-terminal gnome-keyring nitrogen
+yay -S i3 dunst picom udisks2 thunar gvfs thunar-archive-plugin xarchiver mate-polkit \
+  ffmpegthumbnailer tumbler libgsf eog feh xorg-xinput flameshot nwg-look pavucontrol \
+  clipmenu xfce4-terminal gnome-keyring
