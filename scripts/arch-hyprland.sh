f0bbb67f4bb45bb73f54dfa5c5c37755c0530d9e
cusniwtt <windywayu2000@gmail.com>
2026-01-12 01:08:02 +0700

    add nwg-dock-hyprland

--- scripts/arch-hyprland.sh
+++ scripts/arch-hyprland.sh
@@ -2,5 +2,6 @@
 
 yay -S hyprland hyprpicker hyprlock xdg-desktop-portal-hyprland hyprpolkitagent \
   swww mako waypaper wlogout gnome-terminal waybar cliphist wofi grim slurp swappy nwg-look  \
-  qt5-wayland qt6-wayland hyprshade nemo nemo-fileroller nemo-preview eog wireplumber iwdgui
+  qt5-wayland qt6-wayland hyprshade nemo nemo-fileroller nemo-preview eog wireplumber iwdgui \
+  nwg-dock-hyprland
 
