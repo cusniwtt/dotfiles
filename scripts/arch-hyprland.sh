6064a7f639ac3a7d833d2abe7cdaaf9b1b985f03
cusniwtt <windywayu2000@gmail.com>
2026-01-16 22:52:27 +0700

    fix waybar + package jq

--- scripts/arch-hyprland.sh
+++ scripts/arch-hyprland.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yay -S hyprland hyprpicker hyprlock xdg-desktop-portal-hyprland hyprpolkitagent \
+yay -S hyprland hyprpicker hyprlock xdg-desktop-portal-hyprland hyprpolkitagent jq\
   swww mako waypaper wlogout gnome-terminal waybar cliphist wofi grim slurp swappy nwg-look  \
   qt5-wayland qt6-wayland hyprshade nemo nemo-fileroller nemo-preview eog wireplumber iwdgui \
   nwg-dock-hyprland
